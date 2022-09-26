44bbfa0e22259ad2c88835fd56aa76925cd83467
renanguillen <rigcpj@gmail.com>
2022-09-26 21:34:40 +0200

    Automatic commit via Makefile

--- sources/client.c
+++ sources/client.c
@@ -6,13 +6,17 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 16:31:47 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/09/22 20:03:46 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2022/09/26 21:31:25 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	pid_t	pid;
+
+	if (argc != 3)
+		error("Invalid Syntax.\n");
+	else if (ft_strlen(argv[1]))
 }
